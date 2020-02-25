95c2bd42b46bf2b54e5781b3489c7c9e497522aa
carmiccc <carmiccc@LCBLAB2110-08.uccob.uc.edu>
2020-02-25 09:18:20 -0500

    another commit

--- ImageWork/src/image_functions.py
+++ ImageWork/src/image_functions.py
@@ -8,6 +8,7 @@ import os, sys
 #import requests
 from io import BytesIO
 
+
 def load_image( filename ) :
     try:
         myimage = Image.open(filename)
@@ -15,6 +16,21 @@ def load_image( filename ) :
         return myimage
     except:
         print("load_image(): Unable to load")
+        return None
+    
+def save_image( imageObject, outfilename ) :
+    """
+    Save an image to disk
+    :param imageObject: The Image to save
+    :param outfilename: The target file
+    """
+    try:
+        imageObject.save( outfilename )
+    except:
+        print("save_image(): couldn't save" + outfilename)
         
+def crop_image(imageObject, cropRegion):
+    im_c = imageObject.crop((cropRegion)) # (left, top, right, bottom) it's a tuple!
+    return im_c
 
     
--- ImageWork/src/main.py
+++ ImageWork/src/main.py
@@ -8,10 +8,17 @@ from image_functions import *
 im = Image.open("SiriusAndViolet.jpg")
 print(im.width, im.height, im.mode, im.format)  # Display some info about the image
 
-my_image = load_image("xSiriusAndViolet.jpg")
-my_image.show(my_image)
+#my_image = load_image("xSiriusAndViolet.jpg")
+#my_image.show(my_image)
 
+#make use of crop_image
+#1. load image
+#2. call crop image and save what it returns
+#3. call show to display results
 
+im = Image.open("SiriusAndViolet.jpg")  
+im_cropped = crop_image(im, (200,300,400,500))
+im_cropped.show()
 
 
 
